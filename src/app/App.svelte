205477bce5c802ccd7e5d1dbd70aa15701aea3ae
ptazithos <tai.zeming@pithos.net>
2023-04-03 21:40:54 +0800

    Add debug dashboard

--- src/app/App.svelte
+++ src/app/App.svelte
@@ -2,16 +2,10 @@
   import { theme } from "../store/theme";
   import AppContainer from "./features/Provider/AppContainer.svelte";
   import Toolbar from "./features/Toolbar/Toolbar.svelte";
-  import ThemeSwitcher from "./features/Debug/ThemeSwitcher.svelte";
-  import ZoomConfig from "./features/Debug/ZoomConfig.svelte";
-  import LanguageSwitcher from "./features/Debug/LanguageSwitcher.svelte";
-  import MenuTweak from "./features/Debug/FloatTweak.svelte";
+  import DebugDashboard from "./features/Debug/DebugDashboard.svelte";
 </script>
 
 <AppContainer>
   <Toolbar />
-  <ThemeSwitcher />
-  <LanguageSwitcher />
-  <ZoomConfig />
-  <MenuTweak />
+  <DebugDashboard />
 </AppContainer>
---
+++ src/app/features/Debug/DebugDashboard.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import ThemeSwitcher from "./ThemeSwitcher.svelte";
+  import ZoomConfig from "./ZoomConfig.svelte";
+  import LanguageSwitcher from "./LanguageSwitcher.svelte";
+  import MenuTweak from "./FloatTweak.svelte";
+</script>
+
+<div class="flex flex-wrap">
+  <ThemeSwitcher />
+  <LanguageSwitcher />
+  <ZoomConfig />
+  <MenuTweak />
+</div>
