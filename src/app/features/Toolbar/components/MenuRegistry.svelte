265fa9f798d930623436d967c5cc8f293c3f7c6b
ptazithos <tai.zeming@pithos.net>
2023-04-04 22:20:31 +0800

    Menu config interface

---
+++ src/app/features/Toolbar/components/MenuRegistry.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  interface Item {
+    name: string;
+    action: () => void;
+  }
+
+  interface SubMenu {
+    name: string;
+    items: Item[];
+  }
+
+  interface RegistryConfig {
+    name: string;
+    items: (Item | SubMenu)[];
+  }
+
+  const REGISTRY_CONFIGS: RegistryConfig[] = [];
+</script>
