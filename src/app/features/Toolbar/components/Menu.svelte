ec8e2807d97828b25e9295e86bab8ad114121697
ptazithos <tai.zeming@pithos.net>
2023-04-06 23:04:12 +0800

    Menu style

--- src/app/features/Toolbar/components/Menu.svelte
+++ src/app/features/Toolbar/components/Menu.svelte
@@ -1,1 +1,5 @@
-<div>Hello world</div>
+<div class="bg-$background-1 p-1 border-$background-2 border-2">
+  <div class=" hover:bg-$highlight-1 px-6 text-$font-highlight">
+    Hello world
+  </div>
+</div>
