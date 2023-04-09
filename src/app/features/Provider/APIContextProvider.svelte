c2c31149c6ec056173bb97a290fbd3a6941060d3
ptazithos <tai.zeming@pithos.net>
2023-04-09 10:32:07 +0800

    Detect current running environment

---
+++ src/app/features/Provider/APIContextProvider.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { setContext } from "svelte";
+
+  setContext("native_api", {});
+
+  if (!!window.__TAURI__) {
+    // code is running in tauri environment
+    console.log("Tauri environment detected");
+  } else {
+    // code may be running in a browser
+    console.log("Browser environment detected");
+  }
+</script>
+
+<slot />
--- src/app/features/Provider/AppContainer.svelte
+++ src/app/features/Provider/AppContainer.svelte
@@ -2,12 +2,15 @@
   import ThemeProvider from "./ThemeProvider.svelte";
   import I18nProvider from "./I18nProvider.svelte";
   import FloatContentProvider from "./FloatContentProvider.svelte";
+  import ApiContextProvider from "./APIContextProvider.svelte";
 </script>
 
-<ThemeProvider>
-  <I18nProvider>
-    <FloatContentProvider>
-      <slot />
-    </FloatContentProvider>
-  </I18nProvider>
-</ThemeProvider>
+<ApiContextProvider>
+  <ThemeProvider>
+    <I18nProvider>
+      <FloatContentProvider>
+        <slot />
+      </FloatContentProvider>
+    </I18nProvider>
+  </ThemeProvider>
+</ApiContextProvider>
