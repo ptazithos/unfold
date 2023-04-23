c9c171e8d6f2130c22c214f005b978e0631b15c9
ptazithos <tai.zeming@pithos.net>
2023-04-23 23:56:42 +0800

    Use css variable

--- src/app/features/Provider/AppContainer/components/loading.svelte
+++ src/app/features/Provider/AppContainer/components/loading.svelte
@@ -3,6 +3,6 @@
 >
   <i
     class="fa-duotone fa-spinner-third fa-spin fa-2xl"
-    style="--fa-primary-color: #edda63; --fa-secondary-color: #edda63;"
+    style="--fa-primary-color: var(--primary-1); --fa-secondary-color: var(--primary-1);"
   />
 </div>
