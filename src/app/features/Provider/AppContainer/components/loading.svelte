9c0c46564338b5b09f1983be6110499af81f1268
ptazithos <tai.zeming@pithos.net>
2023-04-24 00:05:56 +0800

    Shadow full screen

--- src/app/features/Provider/AppContainer/components/loading.svelte
+++ src/app/features/Provider/AppContainer/components/loading.svelte
@@ -1,6 +1,4 @@
-<div
-  class="fixed h-full w-full flex items-center justify-center bg-opacity-75 bg-dark-900"
->
+<div class="fixed h-full w-full flex items-center justify-center bg-dark-900">
   <i
     class="fa-duotone fa-spinner-third fa-spin fa-2xl"
     style="--fa-primary-color: var(--primary-1); --fa-secondary-color: var(--primary-1);"
