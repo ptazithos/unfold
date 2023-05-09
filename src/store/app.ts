65007bf97083fec3e0e469b25059b10e74cefdc1
ptazithos <tai.zeming@pithos.net>
2023-05-09 22:39:07 +0800

    App store init

--- src/store/app.ts
+++ src/store/app.ts
@@ -1,4 +1,4 @@
-import { writable } from "svelte/store";
+import { derived, writable } from "svelte/store";
 
 export enum AppStatus {
   BEFORE_LOAD = 0,
@@ -6,5 +6,39 @@ export enum AppStatus {
   LOADED = 2,
 }
 
-export const appStatus = writable(AppStatus.BEFORE_LOAD);
-export const scenarios = writable({});
+type APPStore = {
+  status: AppStatus;
+  scenario: any;
+};
+
+const APP_DEFAULT_STORE: APPStore = {
+  status: AppStatus.BEFORE_LOAD,
+  scenario: null,
+};
+
+function createAppStore() {
+  const { subscribe, set, update } = writable<APPStore>(APP_DEFAULT_STORE);
+
+  const completeLoading = (scenario: any) => {
+    set({ status: AppStatus.LOADED, scenario });
+  };
+
+  const failLoading = () => {
+    set({ status: AppStatus.BEFORE_LOAD, scenario: null });
+  };
+
+  const startLoading = () => {
+    set({ status: AppStatus.LOADING, scenario: null });
+  };
+
+  return {
+    subscribe,
+    completeLoading,
+    failLoading,
+    startLoading,
+  };
+}
+
+export const appStore = createAppStore();
+export const appStatus = derived(appStore, ($appStore) => $appStore.status);
+export const scenario = derived(appStore, ($appStore) => $appStore.scenario);
