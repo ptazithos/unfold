d287b564cc4d322560f62032e30f0d8b0b4e4a57
ptazithos <tai.zeming@pithos.net>
2023-05-14 23:59:01 +0800

    fix: remove unused import

--- src/app/features/Provider/AppContainer/index.svelte
+++ src/app/features/Provider/AppContainer/index.svelte
@@ -3,8 +3,6 @@
   import I18nProvider from "../I18nProvider.svelte";
   import FloatContentProvider from "../FloatContentProvider.svelte";
   import ApiContextProvider from "../APIProvider/index.svelte";
-
-  import { status } from "unfold/store/pretask";
 </script>
 
 <ThemeProvider>
