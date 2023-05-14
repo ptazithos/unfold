0e43c8b068666dc1aae4da9400c8d3ec12ff6251
ptazithos <tai.zeming@pithos.net>
2023-05-14 23:56:36 +0800

    feat: eslint ts support

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,8 @@ module.exports = {
     "browser": true,
     "node": true
   },
-  extends: ["plugin:svelte/recommended", "plugin:svelte/prettier", 'eslint:recommended'],
+  plugins:["@typescript-eslint"],
+  extends: ["plugin:svelte/recommended", "plugin:svelte/prettier", "eslint:recommended"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     //project: "tsconfig.json",
@@ -18,4 +19,8 @@ module.exports = {
       },
     },
   ],
+  rules: {
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "error"
+  }
 }
