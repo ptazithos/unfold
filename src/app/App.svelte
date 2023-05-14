c33558bee140cf4bf9025d7246efe57159068f20
ptazithos <tai.zeming@pithos.net>
2023-05-14 23:38:44 +0800

    fix: remove unused splitpanes

--- src/app/App.svelte
+++ src/app/App.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import { Pane, Splitpanes } from "svelte-splitpanes";
   import AppContainer from "./features/Provider/AppContainer/index.svelte";
   import Toolbar from "./features/Toolbar/Toolbar.svelte";
   import Layout from "./features/Layout/index.svelte";
