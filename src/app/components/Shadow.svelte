7293eb27643eba4a8a372158240fafa7265f7315
ptazithos <tai.zeming@pithos.net>
2023-05-15 00:06:04 +0800

    fix: loading screen should be transparent

--- src/app/components/Shadow.svelte
+++ src/app/components/Shadow.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
   export let position: "fixed" | "absolute";
-  export let opacity = 50;
+  export let transparent = true;
 </script>
 
 <div
-  class={`${position} inset-0 bg-black opacity-${opacity} flex justify-center items-center font-bold text-2xl`}
+  class={`${position} inset-0 bg-black ${
+    transparent ? "opacity-50" : ""
+  } flex justify-center items-center font-bold text-2xl`}
 >
   <slot />
 </div>
--- src/app/features/Provider/APIProvider/components/SplashScreen.svelte
+++ src/app/features/Provider/APIProvider/components/SplashScreen.svelte
@@ -2,7 +2,7 @@
   import Shadow from "unfold/app/components/Shadow.svelte";
 </script>
 
-<Shadow position="fixed" opacity={100}>
+<Shadow position="fixed" transparent={false}>
   <i
     class="fa-duotone fa-spinner-third fa-spin fa-xl"
     style="--fa-primary-color: var(--primary-1); --fa-secondary-color: var(--primary-1);"
