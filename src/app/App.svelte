2179d104172cc028b8351a66db8970f8b7225f65
ptazithos <tai.zeming@pithos.net>
2023-05-22 15:39:53 +0800

    feat: explorer title

--- src/app/App.svelte
+++ src/app/App.svelte
@@ -3,11 +3,12 @@
   import Toolbar from "./features/Toolbar/Toolbar.svelte";
   import Layout from "./features/Layout/index.svelte";
   import DebugDashboard from "./features/Debug/DebugDashboard.svelte";
+  import Structure from "./features/Structure/index.svelte";
   import Overlay from "./features/Overlay.svelte";
 </script>
 
 <AppContainer>
   <Toolbar />
-  <Layout><DebugDashboard slot="main" /></Layout>
+  <Layout><DebugDashboard slot="main" /><Structure slot="side" /></Layout>
   <Overlay />
 </AppContainer>
---
+++ src/app/features/Structure/index.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import { _ } from "svelte-i18n";
+</script>
+
+<div class="w-full h-6 px-4 pt-1 text-sm text-$font-disabled">
+  {$_("structure.title")}
+</div>
