3b00f71ec6804ffde4471cae25f6e6a9a5b4cca2
ptazithos <tai.zeming@pithos.net>
2023-05-22 22:06:22 +0800

    feat: TreeElement for Tree view

---
+++ src/app/features/Structure/components/TreeElement.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import { type TreeElement, ElementType } from "./type";
+  import TreeItem from "./TreeItem.svelte";
+  import TreeSet from "./TreeSet.svelte";
+
+  export let config: TreeElement;
+  const { label, type } = config;
+
+  let component = null;
+  $: {
+    switch (type) {
+      case ElementType.Item:
+        component = TreeItem;
+        break;
+      case ElementType.Set:
+        component = TreeSet;
+        break;
+      default:
+        component = null;
+    }
+  }
+</script>
+
+<div>
+  <p>{label}</p>
+  {#if component}
+    <svelte:component this={component} />
+  {:else}
+    <p>No component selected</p>
+  {/if}
+</div>
---
+++ src/app/features/Structure/components/type.ts
@@ -0,0 +1,20 @@
+export enum ElementType {
+  Set = "set",
+  Item = "item",
+}
+
+export type TreeCommon = {
+  label: string;
+  type: ElementType;
+};
+
+export type TreeItem = {
+  type: ElementType.Item;
+} & TreeCommon;
+
+export type TreeSet = {
+  type: ElementType.Set;
+  children: TreeElement[];
+} & TreeCommon;
+
+export type TreeElement = TreeSet | TreeItem;
