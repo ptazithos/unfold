e4889987ea7d3728913b54ce2540c813bf561f62
ptazithos <tai.zeming@pithos.net>
2023-05-22 22:12:19 +0800

    feat: use icon to replace text

--- src/app/components/Loading.svelte
+++ src/app/components/Loading.svelte
@@ -1,5 +1,4 @@
-<script>
-  import { _ } from "svelte-i18n";
-</script>
-
-<p>{$_("loading")}</p>
+<i
+  class="fa-duotone fa-spinner-third fa-spin fa-xl"
+  style="--fa-primary-color: var(--primary-1); --fa-secondary-color: var(--primary-1);"
+/>
