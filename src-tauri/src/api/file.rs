b3bef60b87446880f0e7f3f2a7fabab5aa8fb586
ptazithos <tai.zeming@pithos.net>
2023-05-22 22:31:23 +0800

    fix: avoid UI freeze by using async

--- src-tauri/src/api/file.rs
+++ src-tauri/src/api/file.rs
@@ -1,13 +1,13 @@
 use aoe2_probe::{parse::Token, ExportFormat, Scenario};
 
 #[tauri::command]
-pub fn open_scenario(path: String) -> String {
+pub async fn open_scenario(path: String) -> String {
     let scenario = Scenario::from_file(&path).unwrap();
     serde_json::to_string(&scenario.versio).unwrap()
 }
 
 #[tauri::command]
-pub fn save_scenario(raw: String, path: String, format: ExportFormat) {
+pub async fn save_scenario(raw: String, path: String, format: ExportFormat) {
     let versio: Token = serde_json::from_str(&raw).unwrap();
     let scenario = Scenario::from_versio(&versio).unwrap();
 
