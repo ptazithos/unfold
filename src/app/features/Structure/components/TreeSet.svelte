c60015fc14c7d158926926ab6f93c6adc9ac38ae
ptazithos <tai.zeming@pithos.net>
2023-05-23 22:56:13 +0800

    feat: TreeSet

---
+++ src/app/features/Structure/components/TreeSet.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import TreeElement from "./TreeElement.svelte";
+  import type { TreeSet } from "./type";
+
+  export let config: TreeSet;
+  const { label, elements } = config;
+</script>
+
+<div>
+  {label}
+  {#each elements as eleConfig}
+    <TreeElement config={eleConfig} />
+  {/each}
+</div>
